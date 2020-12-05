37177995de6b7d32048ca7eafce8ad152d7a9d4f
Sanket Badjate <32664738+sanbad36@users.noreply.github.com>
2020-12-05 12:41:20 +0530

    Add files via upload

--- palindrome.c
+++ palindrome.c
@@ -1,25 +1,26 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
+#include <stdio.h>
+int main()
+{
+    int n, reversedInteger = 0, remainder, originalInteger;
 
+    printf("Enter an integer: ");
+    scanf("%d", &n);
 
-char a[20];
-gets(a);
-int i;
-int len=strlen(a);
-for(i=0;i<len/2;i++){
+    originalInteger = n;
 
-	if(a[i]!=a[len-1-i])
-	{
-		printf("Not palindromme");
-		break;
-	}
+    // reversed integer is stored in variable 
+    while( n!=0 )
+    {
+        remainder = n%10;
+        reversedInteger = reversedInteger*10 + remainder;
+        n /= 10;
+    }
 
-
-}
-if(i==len/2);
-printf("palindrome");
-
-
-
+    // palindrome if orignalInteger and reversedInteger are equal
+    if (originalInteger == reversedInteger)
+        printf("%d is a palindrome.", originalInteger);
+    else
+        printf("%d is not a palindrome.", originalInteger);
+    
+    return 0;
 }
